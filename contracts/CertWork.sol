eef930cb092fd18bfcab869bdae14120e2490fbe
proxyGrit <dev@proxygrit.com>
2022-03-13 13:23:15 -0700

    Add 'react-ipfs-uploaded' dependency and 'Proposal' struct

--- contracts/CertWork.sol
+++ contracts/CertWork.sol
@@ -1,6 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+contract CertWork {
+  struct Proposal {
+    string description;
+    uint256 goalAmount;
+    uint256 totalRaised;
+    mapping(address => uint256) donations;
+    string bronzeMetaURI;
+    string silverMetaURI;
+    string goldMetaURI;
+    address[] bronzeDonors;
+    address[] silverDonnors;
+    address[] goldDonors;
+  }
 
-contract CertWork is ERC1155 {}
+  Proposal[] public proposals;
+
+  function proposalCount() external view returns (uint256) {
+    return proposals.length;
+  }
+
+  constructor() {}
+}
